933c05ddcf05aecca234cf038ccc6282bed6f006
nezam0003 <fhameedk.nezam@gmail.com>
2021-01-21 18:56:47 +0600

    initial setup and workflow

--- geekyshow/my-app/src/App.js
+++ geekyshow/my-app/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component, Fragment } from "react";
+// class App extends Component {
+//   render() {
+//     return (
+//       <Fragment>
+//         <h2> Hello React </h2>
+//         <h2> Hello WOrld </h2>
+//       </Fragment>
+//     );
+//   }
+// }
+// export default App;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* ******** Function and class Component *********** */
+
+// ***** Function compunent ****
+// import React from "react";
+// const Student = (props) => {
+//   return <h1>Hello {props.name}</h1>;
+// };
+
+// export default Student;
+
+// Class component
+import React, { Component } from "react";
+class Student extends Component {
+  render() {
+    return <h1>hello{this.props.name}</h1>;
+  }
 }
-
-export default App;
+export default Student;
