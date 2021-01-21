86e8b1e3817027a0e34fcba15c83bc1017178475
nezam0003 <fhameedk.nezam@gmail.com>
2021-01-21 21:26:51 +0600

    JSX with Attributes done

--- geekyshow/my-app/src/index.js
+++ geekyshow/my-app/src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from "react-dom";
 // import App from "./App";
 
 /* ******** Function and class Component *********** */
-import App from "./App";
+// import App from "./App";
+// ReactDOM.render(<App />, document.getElementById("root"));
 
-ReactDOM.render(<App />, document.getElementById("root"));
+/* ******** JSX *********** */
+// import el from "./JsX";
+
+// Rendering Element
+// ReactDOM.render(el, document.getElementById("root"));
+
+/* ******** JSX with Attributes *********** */
+import el from "./JsX";
+ReactDOM.render(el, document.getElementById("root"));
