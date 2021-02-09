35009c5647eab5f9050e8367920e1c42acfa087c
nezam0003 <fhameedk.nezam@gmail.com>
2021-02-09 19:36:44 +0600

    Add, Select and Complete Todo Functionalities

--- Stack-Learner/my-app-two/src/Todo/components/todos/Index.jsx
+++ Stack-Learner/my-app-two/src/Todo/components/todos/Index.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
+import { Modal, ModalBody, ModalHeader } from "reactstrap";
+import shortid from "shortid";
 import ListView from "../list-view";
 import TableView from "../table-view";
+import CreateTodoForm from "../todo-form/index";
+import Controller from "../controllers/index";
 
 export default class Todos extends Component {
   state = {
@@ -22,19 +26,50 @@ export default class Todos extends Component {
         isSelect: false,
       },
     ],
+    isOpenTodoForm: false,
+    searchTerm: "",
   };
 
   toggleSelect = (todoId) => {
-    console.log("hello selecet");
+    const todos = [...this.state.todos];
+    const todo = todos.find((t) => t.id === todoId);
+    todo.isSelect = !todo.isSelect;
+    this.setState({ todos });
   };
   toggleComplete = (todoId) => {
-    console.log("hello complete");
+    const todos = [...this.state.todos];
+    const todo = todos.find((t) => t.id === todoId);
+    todo.isComplete = !todo.isComplete;
+    this.setState({ todos });
+  };
+  toggleForm = (e) => {
+    this.setState({
+      isOpenTodoForm: !this.state.isOpenTodoForm,
+    });
+  };
+  handleSearch = (e) => {
+    console.log("search");
+  };
+  createToDo = (todo) => {
+    todo.id = shortid.generate();
+    todo.time = new Date();
+    todo.isComplete = false;
+    todo.isSelect = false;
+
+    const todos = [todo, ...this.state.todos];
+    this.setState({ todos });
+    this.toggleForm();
   };
 
   render() {
     return (
       <div>
         <h1 className="display-4 mb-5 text-center">My Todos</h1>
+        <Controller
+          term={this.state.searchTerm}
+          handleSearch={this.handleSearch}
+          toggleForm={this.toggleForm}
+        />
         <div className="mb-5">
           <ListView
             todos={this.state.todos}
@@ -49,6 +84,12 @@ export default class Todos extends Component {
             toggleComplete={this.toggleComplete}
           />
         </div>
+        <Modal isOpen={this.state.isOpenTodoForm} toggle={this.toggleForm}>
+          <ModalHeader toggle={this.toggleForm}>Create New Task</ModalHeader>
+          <ModalBody>
+            <CreateTodoForm createToDo={this.createToDo} />
+          </ModalBody>
+        </Modal>
       </div>
     );
   }
