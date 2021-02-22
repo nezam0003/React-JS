dee5d263f2873857485945928fa83ef83a42a216
nezam0003 <fhameedk.nezam@gmail.com>
2021-02-23 00:36:30 +0600

    simple event trail

--- geekyshow/my-app/src/index.js
+++ geekyshow/my-app/src/index.js
@@ -2,7 +2,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// State in React
-import App from "./State/App";
+/************* State in React *********/
+// import App from "./State/App";
+
+/************* Event in React *********/
+import App from "./Event/App";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
