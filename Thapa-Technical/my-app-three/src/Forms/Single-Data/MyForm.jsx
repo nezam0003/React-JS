f57dfccbea1e52990aed41e586fa47c7ab79306d
nezam0003 <fhameedk.nezam@gmail.com>
2021-02-28 20:50:01 +0600

    React Form - Handling Multiple Input

--- Thapa-Technical/my-app-three/src/Forms/Single-Data/MyForm.jsx
+++ Thapa-Technical/my-app-three/src/Forms/Single-Data/MyForm.jsx
@@ -1,23 +1,54 @@
 import React, { useState } from "react";
-import { Button, Input } from "reactstrap";
+import { Form, Button, Input } from "reactstrap";
 import classes from "../../Style.module.css";
 
 const MyForm = () => {
-  const [name, setName] = useState();
-  const [showName, setShowName] = useState();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const [showFirstName, setshowFirstName] = useState();
+  const [showLastName, setShowLastName] = useState();
+
   const inputFieldhandller = (e) => {
-    setName(e.target.value);
+    setFirstName(e.target.value);
   };
-  const showDataOnClick = () => setShowName(name);
+
+  const lastNameHandller = (e) => {
+    setLastName(e.target.value);
+  };
+
+  const showDataOnSubmit = (e) => {
+    e.preventDefault();
+    setshowFirstName(firstName);
+    setShowLastName(lastName);
+  };
+
   return (
     <div className={classes.mainContent}>
       <h4 className="display-4 text-warning">
-        Hello, <span className="text-white">{showName}</span>
+        Hello,
+        <span className="text-white">
+          {showFirstName} {showLastName}
+        </span>
       </h4>
-      <Input type="text" placeholder="Enter your Name" value={name} onChange={inputFieldhandller} />
-      <Button onClick={showDataOnClick} type="submit" color="success" className="mt-3">
-        Submit
-      </Button>
+      <Form onSubmit={showDataOnSubmit}>
+        <Input
+          type="text"
+          placeholder="Enter your First Name"
+          value={firstName}
+          onChange={inputFieldhandller}
+        />
+        <Input
+          className="mt-2"
+          type="text"
+          placeholder="Enter your Last Name"
+          value={lastName}
+          onChange={lastNameHandller}
+        />
+        <Button type="submit" color="success" className="mt-3">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 };
