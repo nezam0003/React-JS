6557e2ec89dda3ae3f586c3159fa38c1fb0cd355
nezam0003 <fhameedk.nezam@gmail.com>
2021-03-01 14:31:07 +0600

    React Forms - add Email and phone

--- Thapa-Technical/my-app-three/src/Forms/Single-Data/MyForm.jsx
+++ Thapa-Technical/my-app-three/src/Forms/Single-Data/MyForm.jsx
@@ -6,21 +6,40 @@ const MyForm = () => {
   const [fullName, setFullName] = useState({
     firstName: "",
     lastName: "",
+    email: "",
+    phone: "",
   });
 
   const inputFieldhandller = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
+    const { name, value } = e.target;
     setFullName((prevValue) => {
       if (name === "firstName") {
         return {
           firstName: value,
           lastName: prevValue.lastName,
+          email: prevValue.email,
+          phone: prevValue.phone,
         };
       } else if (name === "lastName") {
         return {
           firstName: prevValue.firstName,
           lastName: value,
+          email: prevValue.email,
+          phone: prevValue.phone,
+        };
+      } else if (name === "email") {
+        return {
+          firstName: prevValue.firstName,
+          lastName: prevValue.lastName,
+          email: value,
+          phone: prevValue.phone,
+        };
+      } else if (name === "phone") {
+        return {
+          firstName: prevValue.firstName,
+          lastName: prevValue.lastName,
+          email: prevValue.email,
+          phone: value,
         };
       }
     });
@@ -35,13 +54,15 @@ const MyForm = () => {
 
   return (
     <div className={classes.mainContent}>
-      <h4 className="display-4 text-warning">
+      <h4 className="display-4 text-warning d-block text-center">
         Hello,
         <span className="text-white">
           {fullName.firstName} {fullName.lastName}
         </span>
       </h4>
-      <Form onSubmit={showDataOnSubmit}>
+      <p>{fullName.email}</p>
+      <p>{fullName.phone}</p>
+      <Form onSubmit={showDataOnSubmit} className={classes.myForm}>
         <Input
           type="text"
           placeholder="Enter your First Name"
@@ -55,6 +76,22 @@ const MyForm = () => {
           placeholder="Enter your Last Name"
           value={fullName.lastName}
           name="lastName"
+          onChange={inputFieldhandller}
+        />
+        <Input
+          className="mt-2"
+          type="email"
+          placeholder="Enter your Your Email"
+          value={fullName.email}
+          name="email"
+          onChange={inputFieldhandller}
+        />
+        <Input
+          className="mt-2"
+          type="number"
+          placeholder="Enter your phone number"
+          value={fullName.phone}
+          name="phone"
           onChange={inputFieldhandller}
         />
         <Button type="submit" color="success" className="mt-3">
