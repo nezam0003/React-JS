93fe5b403a842cca494f3c7f6b00519e3584a9b6
nezam0003 <fhameedk.nezam@gmail.com>
2021-03-28 12:06:14 +0600

    RoomsContainer done

---
+++ Coding-Addict/hotel-reservation/src/components/RoomsFilter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RoomsFilter = () => {
+  return <div>hello from RoomsFilter</div>;
+};
+
+export default RoomsFilter;
---
+++ Coding-Addict/hotel-reservation/src/components/RoomsList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RoomsList = () => {
+  return <div>hello from RoomList</div>;
+};
+
+export default RoomsList;
