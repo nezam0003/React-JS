4d6302ac284946761498ec13356ab820e15f7b83
nezam0003 <fhameedk.nezam@gmail.com>
2021-03-29 11:50:20 +0600

    RoomsFilter : filtered by type of room

--- Coding-Addict/hotel-reservation/src/Context.jsx
+++ Coding-Addict/hotel-reservation/src/Context.jsx
@@ -8,18 +8,34 @@ class RoomProvider extends Component {
     sortedRooms: [],
     featuredRooms: [],
     loading: true,
+    type: "all",
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false,
   };
 
   componentDidMount() {
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter((room) => room.featured === true);
+    let maxPrice = Math.max(...rooms.map((item) => item.price));
+    let maxSize = Math.max(...rooms.map((item) => item.size));
+
     this.setState({
       rooms,
       featuredRooms,
       sortedRooms: rooms,
       loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize,
     });
   }
+
   formatData(items) {
     let tempItems = items.map((item) => {
       let id = item.sys.id;
@@ -37,12 +53,43 @@ class RoomProvider extends Component {
     return room;
   };
 
+  handleChange = (e) => {
+    const target = e.target;
+    const value = e.type === "checkbox" ? target.checked : target.value;
+    const name = e.target.name;
+    this.setState(
+      {
+        [name]: value,
+      },
+      this.filterRooms
+    );
+  };
+
+  filterRooms = () => {
+    let {
+      rooms,
+      type,
+      capacity,
+      price,
+      minSize,
+      maxSize,
+      breakfast,
+      pets,
+    } = this.state;
+    let tempRooms = [...rooms];
+    if (type !== "all") {
+      tempRooms = tempRooms.filter((room) => room.type === type);
+    }
+    this.setState({ sortedRooms: tempRooms });
+  };
+
   render() {
     return (
       <RoomContext.Provider
         value={{
           ...this.state,
           getRoom: this.getRoom,
+          handleChange: this.handleChange,
         }}
       >
         {this.props.children}
--- Coding-Addict/hotel-reservation/src/components/RoomsFilter.jsx
+++ Coding-Addict/hotel-reservation/src/components/RoomsFilter.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useContext } from "react";
+import { RoomContext } from "../Context";
+import Title from "./Title";
 
-const RoomsFilter = () => {
-  return <div>hello from RoomsFilter</div>;
+// Get unique values
+const getUnique = (items, value) => {
+  return [...new Set(items.map((item) => item[value]))];
+};
+
+const RoomsFilter = ({ rooms }) => {
+  const context = useContext(RoomContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets,
+  } = context;
+
+  // get unique type
+  let types = getUnique(rooms, "type");
+  // add all
+  types = ["all", ...types];
+  types = types.map((item, index) => {
+    return (
+      <option value={item} key={index}>
+        {item}
+      </option>
+    );
+  });
+  return (
+    <section className="filter-container">
+      <Title title="search room" />
+      <form className="filter-form">
+        {/* select type */}
+        <div className="form-group">
+          <label htmlFor="type">room type</label>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
+        </div>
+        {/* select type end */}
+      </form>
+    </section>
+  );
 };
 
 export default RoomsFilter;
